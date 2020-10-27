9ca9ce8ba944900a4c6f5d53a629574191de0e24
JosephBarr Uni <n10469826@qut.edu.au>
2020-10-28 02:33:00 +1000

    Added custom-env to package.json Added custom-env code to App App now listens for .env.* files for configuration. Will use current environment (Eg. NODE_ENV=dev)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const customEnv = require('custom-env').env(true)
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
