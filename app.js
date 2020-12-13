06fd088351847381b150226df34d0c6c0c9baa79
Joseph Barr <jjbarr10@icloud.com>
2020-12-14 02:58:48 +1000

    Added CORS function to fix cors issues on the remote server

--- app.js
+++ app.js
@@ -30,7 +30,14 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(helmet());
+
+// CORS Stuff
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
